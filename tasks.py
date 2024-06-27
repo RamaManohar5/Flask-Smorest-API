782ea74893bafb39d6ba291c48c0bd841c6955b2
ramamanohar5 <ramamanohar.kamarsa@gmail.com>
2024-06-28 01:27:09 +0530

    html response added to user registration

--- tasks.py
+++ tasks.py
@@ -7,17 +7,24 @@ from settings import REDIS_URL,QUEUES
 import redis
 from rq import Queue
 
+import jinja2
+
 load_dotenv()
 
 # Establish connection to Redis
 connection = redis.from_url(REDIS_URL)
-
 # Initialize Queue for emails
 queue = Queue(QUEUES[0], connection=connection)
-
 DOMAIN = os.getenv("MAILGUN_DOMAIN")
 
-def send_simple_message(to, subject, body):
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.environment(loader=template_loader)
+
+
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+def send_simple_message(to, subject, body, html):
     api_key = os.getenv("MAILGUN_API_KEY")
     print("came to simple message")
     return requests.post(
@@ -27,14 +34,16 @@ def send_simple_message(to, subject, body):
             "from": f"RamaManohar <mailgun@{DOMAIN}>",
             "to": [to],
             "subject": subject,
-            "text": body
+            "text": body,
+            "html" : html,
         }
     )
 
 def send_user_registration_message(email, username):
     print("came to nested user registration message")
     send_simple_message(
-        to = email,
-        subject = "Successfully registered",
-        body = f"Hi {username}, you have successfully signed up to the stores REST API"
+        email,
+        "Successfully registered",
+        f"Hi {username}, you have successfully signed up to the stores REST API",
+        render_template("email/registration.html", username=username),
     )
